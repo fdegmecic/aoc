8255ac686fc8eaa58ba16d4266cf66dd2a06da80
fdegmecic <filip_degmecic@yahoo.com>
2023-12-04 21:53:34 +0100

    day4

---
+++ 2023/rust/day-04/src/part1.rs
@@ -0,0 +1,54 @@
+use std::collections::HashSet;
+
+pub fn process(input: &str) -> u32 {
+    let mut result = vec![];
+    for line in input.lines() {
+        let (_, rest) = line.split_once(':').unwrap();
+
+        let (winning, numbers) = rest
+            .split_once('|')
+            .map(|(winning, numbers)| {
+                let winning_set: HashSet<u32> = winning
+                    .split_whitespace()
+                    .filter_map(|num_str| num_str.parse::<u32>().ok())
+                    .collect();
+
+                let nums_set: HashSet<u32> = numbers
+                    .split_whitespace()
+                    .filter_map(|num_str| num_str.parse::<u32>().ok())
+                    .collect();
+
+                (winning_set, nums_set)
+            })
+            .unwrap();
+
+        result.push((winning, numbers))
+    }
+
+    let res = result
+        .iter()
+        .filter_map(|(win, num)| {
+            let power = win.intersection(&num).count() as u32;
+
+            power.checked_sub(1).map(|num| 2u32.pow(num))
+        })
+        .sum::<u32>();
+
+    res as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        assert_eq!(13, process(input));
+    }
+}
