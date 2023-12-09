5dddacd6482270e4373e341b2fac1dc2f20fb028
fdegmecic <filip_degmecic@yahoo.com>
2023-12-09 23:15:05 +0100

    day9

---
+++ 2023/rust/day-09/src/part2.rs
@@ -0,0 +1,55 @@
+use itertools::{Itertools, Position};
+use nom::character::complete::newline;
+use nom::{
+    character::complete::{self, space1},
+    multi::separated_list1,
+    IResult,
+};
+
+fn parse_reports(input: &str) -> IResult<&str, Vec<Vec<i32>>> {
+    separated_list1(newline, separated_list1(space1, complete::i32))(input)
+}
+
+pub fn process(input: &str) -> i32 {
+    let (_, reports) = parse_reports(input).expect("correct parsing");
+
+    reports
+        .iter()
+        .map(|report| {
+            let mut start_nums: Vec<i32> = vec![];
+            let mut nums: Vec<i32> = report.to_vec();
+            loop {
+                if nums.iter().all(|num| num == &0) {
+                    break;
+                }
+                nums = nums
+                    .iter()
+                    .tuple_windows::<(&i32, &i32)>()
+                    .with_position()
+                    .map(|(position, (left, right))| {
+                        match position {
+                            Position::First | Position::Only => start_nums.push(*left),
+                            _ => {}
+                        }
+
+                        right - left
+                    })
+                    .collect::<Vec<i32>>();
+            }
+            start_nums.iter().rev().fold(0, |acc, num| num - acc)
+        })
+        .sum::<i32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(2, process(input));
+    }
+}
